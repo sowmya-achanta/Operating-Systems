b59f28380353469417e2011ab0f28fba3776525d
omachant <omachant@indiana.edu>
2015-09-30 21:30:33 -0400

    Assignment 3- First commit

--- bbb-xinu/include/prodcons.h
+++ bbb-xinu/include/prodcons.h
@@ -1,12 +1,16 @@
+#include <stdio.h>
 #include <stddef.h>
-#include <stdio.h> 
 
 
 
-/*Global variable for producer consumer*/
-  extern int n; /*this is just declaration*/
 
-/*function Prototype*/
-  void consume(int count);
 
-  void produce(int count);
+   /*Global variable for producer consumer*/
+   extern int n; /*this is just declaration*/
+
+   /* Declare the required semaphores */
+   extern sid32 consumed, produced;   
+
+   /*function Prototype*/
+   void consume(int count);
+   void produce(int count);
